628567dd21970b46bbf1851b2ca777b5f854bfd7
aryarafiwww <arya.rafi98@yahoo.co.id>
2017-10-13 09:14:10 +0700

    Form_Edit_Kontak

--- Pert3/form_edit_kontak.php
+++ Pert3/form_edit_kontak.php
@@ -1,11 +1,28 @@
 <?php // filename: form_edit_kontak.php
-
+	include"koneksi.php";
+	
+	$id = $_GET['id'];
+	$query = "SELECT * FROM kontak
+			  WHERE id=$id";
+	$hasil = mysqli_query($db, $query);
+	$row = mysqli_fetch_assoc($hasil)
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Phone Book</title>
+	<title>Data Mahasiswa Kalbis</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" 
+	integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" 
+	crossorigin="anonymous">
+	<style>
+		#tbody{background:#000000;}
+		#menu{background:#CCCCFF;}
+		#konten{background:#CCCC33;}
+		#filter{background:#33FFCC;}
+		#search{background:#33FFCC;}
+		#footer{background:cyan;}
+	</style>
 </head>
 <body>
 <h1>Phone Book</h1>
@@ -17,23 +34,33 @@
 </div>
 <div id="konten">
 	<h2>Edit Kontak</h2>
-	<form action="" method="post">
+	<form action="proses_edit_kontak.php" method="post">
+	<tbody>
 		Nama:
-		<input type="text" name="nama" />
+		<input type="text" value="<?php echo $row['nama']; ?>"name="nama" />
 		<br />
 		Phone:
-		<input type="text" name="phone" />
+		<input type="text" value="<?php echo $row['phone']; ?>"name="phone" />
 		<br />
 		Email:
-		<input type="text" name="email" />
+		<input type="text" value="<?php echo $row['email']; ?>"name="email" />
 		<br />
 		Kategori:
 		<select name="kategori">
-			<option value=""></option>
+		<?php
+		$db = mysqli_connect("localhost", "root", "", "kalbis_new");
+		$query = "select * from kategori";
+		$hasil = mysqli_query($db,$query);
+		while($data=mysqli_fetch_array($hasil)){
+			echo "<option value=$data[id]>$data[keterangan]</option>";
+		}
+		?>
 		</select>
 		<br />
+		<input type="hidden" name="id" value="<?php echo $row['id']; ?>" />
 		<input type="submit" value="Simpan" />
 	</form>
+	</tbody>
 </div>
 </body>
 </html>
