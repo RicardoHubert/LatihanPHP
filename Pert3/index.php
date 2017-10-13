010fa6150a063d1c6a2d6f29f42c3f4779eb9d11
aryarafiwww <arya.rafi98@yahoo.co.id>
2017-10-13 09:56:49 +0700

    Index

--- Pert3/index.php
+++ Pert3/index.php
@@ -10,7 +10,18 @@ $hasil = mysqli_query($db, $query);
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Phone Book</title>
+	<title>Data Mahasiswa Kalbis</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" 
+	integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" 
+	crossorigin="anonymous">
+	<style>
+		#tbody{background:#000000;}
+		#menu{background:#CCCCFF;}
+		#konten{background:#CCCC33;}
+		#filter{background:#33FFCC;}
+		#search{background:#33FFCC;}
+		#footer{background:cyan;}
+	</style>
 </head>
 <body>
 <h1>Phone Book</h1>
@@ -57,14 +68,14 @@ $hasil = mysqli_query($db, $query);
 			$i++;
 		?>
 			<tr>
-				<td><?php echo $i; ?></td>
+				<td><?php echo $row['id']; ?></td>
 				<td><?php echo $row['nama']; ?></td>
 				<td><?php echo $row['phone']; ?></td>
 				<td><?php echo $row['email']; ?></td>
-				<td><?php echo $row['keterangan']; ?></td>
+				<td><?php echo $row['kategori_id']; ?></td>
 				<td>
-					<a href="">Edit</a> | 
-					<a href="">Delete</a>
+				<a href="form_edit_kontak.php?id=<?php echo $row['id'];?>">Edit</a> | 
+					<a href="delete_kontak.php?id=<?php echo $row['id'];?>">Delete</a>
 				</td>
 			</tr>
 		<?php
