9b0936a5d37b2ac8c910fa89e0e0f80528ca8a6b
aryarafiwww <arya.rafi98@yahoo.co.id>
2017-11-10 00:36:12 +0700

    Connecting to MySQL with PHP

---
+++ Pert6Video/database.php
@@ -0,0 +1,31 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "widget_cms";
+$dbpass = "secretpassword";
+$dbname = "widget_corp";
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+if(mysqli_connect_errno()){
+	die("Database connection failed: " .
+	mysqli_connect_error() .
+	"(" .mysqli_connect_errno() . ")"
+	);
+}
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+
+	<html lang="en">
+	<head>
+		<title>Databases</title>
+	</head>
+	<body>
+
+	</body>
+	</html>
+
+	<?php
+	mysqli_close($connection);
+?>
